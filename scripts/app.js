c1289580aede402076d8be9faa6ebbf4b8c755a7
Polidora90 <alessandra.torry@gmail.com>
2021-05-12 22:19:46 +0200

    filtra gli album in base al genere

--- scripts/app.js
+++ scripts/app.js
@@ -2,12 +2,42 @@ new Vue({
     el: "#app",
     data: {
         albumList: [],
+        genres: [],
+        selectedGenre: ""
+    },
+    methods: {
+        setGenre(event) {
+            const select = event.currentTarget;
+            this.selectedGenre = select.value;
+        },
+    },
+    computed: {
+        getAlbums() {
+
+            if (this.selectedGenre === "") {
+                return this.albumList;
+            }
+
+            return this.albumList.filter((album) => {
+                return album.genre == this.selectedGenre;
+            })
+        }
     },
     mounted(){
         axios.get("https://flynn.boolean.careers/exercises/api/array/music")
         .then((resp) => {
 
             this.albumList = resp.data.response;
+                
+            for (let i = 0; i < this.albumList.length; i++) {
+                const element = this.albumList[i];
+
+                if (this.genres.includes(element.genre) == false) {
+                    this.genres.push(element.genre);
+                }
+                
+
+            }
         })
     }
 })
